d98660bb37ad05abbfb8ebfba007a2141e0b2760
Jesus Pastor <jnavalon@biit-solutions.com>
2023-11-17 09:21:16 +0100

    added some basic configuration and dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,18 @@
+import { registerLocaleData } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import localeEn from '@angular/common/locales/en';
+import localeEs from '@angular/common/locales/es';
+import localeNL from '@angular/common/locales/nl';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {TranslocoRootModule} from "biit-ui/i18n";
+
+registerLocaleData(localeEn, 'en')
+registerLocaleData(localeEs, 'es');
+registerLocaleData(localeNL, 'nl');
 
 @NgModule({
   declarations: [
@@ -10,6 +20,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    TranslocoRootModule,
     AppRoutingModule
   ],
   providers: [],
