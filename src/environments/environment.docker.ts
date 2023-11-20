5ace8537af552d5cede3d447343efdc4cf0caeed
Jesus Pastor <jnavalon@biit-solutions.com>
2023-11-20 11:13:36 +0100

    changed docker variable

--- src/environments/environment.docker.ts
+++ src/environments/environment.docker.ts
@@ -1,4 +1,4 @@
 export class Environment {
-  public static readonly ROOT_URL: string = 'PROTOCOL://MACHINE_DOMAIN';
+  public static readonly ROOT_URL: string = `DOCKER_PROTOCOL://DOCKER_MACHINE_DOMAIN`;
   public static readonly APPOINTMENT_CENTER_PATH: string = 'appointment-center-backend';
 }
