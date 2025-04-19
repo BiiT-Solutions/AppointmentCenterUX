7c3ba671df8ea7fedf5828cc764c2768775a8ea6
Jesus Pastor <jnavalon@biit-solutions.com>
2025-04-19 13:43:24 +0200

    fix: javascript takes 00 value as a false

--- src/app/shared/utils/calendar-event-converter.module.ts
+++ src/app/shared/utils/calendar-event-converter.module.ts
@@ -4,6 +4,7 @@ import {NgModule} from "@angular/core";
 import {ColorThemePipeModule} from "../pipes/color-theme-event/color-theme-pipe.module";
 import {ScheduleRange} from "appointment-center-structure-lib/lib/models/schedule-range";
 import { startOfWeek } from 'date-fns'
+import {isNumber} from "@ngneat/transloco";
 
 @NgModule({
   imports: [
@@ -20,9 +21,9 @@ export class CalendarEventConverter {
     let startTime: string[] = scheduleRange.startTime.split(':');
     let endTime: string[] = scheduleRange.endTime.split(':');
     const startEvent: Date = new Date(dateOnWeek);
-    startEvent.setHours(+startTime[0] || 0, +startTime[1] || 0);
+    startEvent.setHours( isNumber(+startTime[0]) ? +startTime[0] : 0, isNumber(+startTime[1]) ? +startTime[1] : 0);
     const endEvent: Date = new Date(dateOnWeek);
-    endEvent.setHours(+endTime[0] || 23, +endTime[1] || 59);
+    endEvent.setHours(isNumber(+endTime[0]) ? +endTime[0] : 23, isNumber(+endTime[1]) ? +endTime[1] : 59);
     return  new CalendarEvent(scheduleRange.id, "",
       startEvent , endEvent, false, EventColor.RED, undefined, false, true);
   }
