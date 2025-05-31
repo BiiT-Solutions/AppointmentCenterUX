74d392995c4a74797ccedd6de5cd17efa87a4872
jpastor <jpastor@localhost>
2025-05-31 18:56:22 +0200

    barred color + disable actions for external events

--- src/app/shared/utils/calendar-event-converter.module.ts
+++ src/app/shared/utils/calendar-event-converter.module.ts
@@ -13,18 +13,24 @@ import {startOfWeek} from 'date-fns'
 export class CalendarEventConverter {
   public static convertToCalendarEvent(appointment: Appointment): CalendarEvent {
     const event: CalendarEvent = new CalendarEvent(appointment.id, appointment.title, appointment.startTime,
-      appointment.endTime, !appointment.calendarProvider,
-      appointment.allDay, (EventColor as any)[appointment.colorTheme], undefined, true, !appointment.calendarProvider, appointment);
+      appointment.endTime, !!appointment.calendarProvider,
+      appointment.allDay, (EventColor as any)[appointment.colorTheme], undefined, !appointment.calendarProvider, !appointment.calendarProvider, appointment);
     if (appointment.calendarProvider) {
       switch (appointment.calendarProvider) {
         case CalendarProvider.MICROSOFT:
-          event.color = EventColor.BLUE;
+          event.color = {
+            primary: '#3f82c2',
+            secondary: '#7eb7e6',
+            hover: '#a7c7e2',
+            tertiary: '#1e4161',
+            barred: true
+          };
           break;
         case CalendarProvider.GOOGLE:
-          event.color = EventColor.YELLOW;
+          event.color = EventColor.BARRED_YELLOW;
           break;
         case CalendarProvider.APPLE:
-          event.color = EventColor.RED;
+          event.color = EventColor.BARRED_GREY;
       }
     }
     return event;
